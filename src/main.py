f21baad1db3c3260d95057f4f7c05545fe5179c4
Rodney McCoy <rbmj2001@outlook.com>
2023-02-28 11:26:00 -0800

    a

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """
 Rodney McCoy
 Thu Oct 13 13:08:41 2022\
-rbmj2001@outlook.com    
+rbmj2001@outlook.com
 """
 
 import hypercube_search as search
@@ -14,27 +14,24 @@ import statistics
 
 
 
-
-
 def calculate(p) -> dict:
     # Calculate Statistics
     pi = sigma.n_cube(data=p)
     rho = is_equal.a_bar(pi)
-    
+
     d_pi = metrics.signed_decomposition(pi)
     d_rho = metrics.signed_decomposition(rho)
-    
+
     k = metrics.K_n(pi)
     # i = metrics.I_n(pi)
     # d = metrics.D_n(pi)
     # ex = metrics.EX_n(pi)
-    
+
     diff = metrics.b_oddness(rho) - metrics.b_oddness(pi)
     #k_rho = metrics.K_n(rho)
+
     
-    
-    
-    
+
     # Test Condition, output to file
     test = is_equal.test(pi)
     success = (test == (k==0))
@@ -42,10 +39,11 @@ def calculate(p) -> dict:
         
     if not success:
         s = "{} {} {} {} {}".format(diff, pi, d_pi, rho, d_rho)
-        # file.write(s + "\n")
+
+        file.write(s + "\n")
         # graph.plot(pi)
-        if diff != 2:
-            print(s)
+        # if diff != 2:
+            # print(s)
 
     return int(success)
     
@@ -57,7 +55,7 @@ def calculate(p) -> dict:
 if(__name__ == "__main__"):
     # Write output to text file
     with open("results.txt", 'w+', encoding='utf-8') as file:
-        low = up = 8
+        low = up = 5 # no CE up to 9
         
         for n in range(low, up+1):
             # Apply calculate to each n dimension hypercube
